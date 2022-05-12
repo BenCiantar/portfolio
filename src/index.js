ef0ed18e2dea41f643b070980e424df2837e4119
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-05-12 11:26:23 +0200

    Added basic components and file system

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import ReactDOM from 'react-dom/client'
+import './styles/styles.css';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      <App />
+  </React.StrictMode>
 );
-
 
