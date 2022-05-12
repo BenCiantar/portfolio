69923a2ed350a18307b29b4a944731af232f5f24
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-05-12 19:33:42 +0200

    Added media query to header to hide nav links and center title on medium screens and smaller

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,11 @@ import { Links } from "../index";
 
 const Header = () => {
   return (
-    <header className="w-screen h-20 bg-primary-purple text-white flex flex-row justify-between items-center px-7">
+    <header className="w-screen h-24 bg-primary-purple text-white flex flex-row justify-center lg:justify-between items-center px-7">
       <h1 className="text-3xl">Hi, I'm Ben</h1>
-      <Links />
+      <div className="hidden lg:block">
+        <Links />
+      </div>
     </header>
   );
 };
