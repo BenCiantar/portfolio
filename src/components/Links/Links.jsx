55f9ecb66610d48790092af3d366e28936c8cdb3
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-05-12 19:42:48 +0200

    Added hover effects and destinations to nav links

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -6,18 +6,18 @@ const Links = () => {
   return (
     <nav>
       <ul className="w-48 flex flex-row justify-evenly items-center">
-        <a href="">
-          <li>
+        <a href="https://www.linkedin.com/in/benciantar/">
+          <li className="hover:text-black">
             <SiLinkedin size="2rem" />
           </li>
         </a>
-        <a href="">
-          <li>
+        <a href="https://github.com/BenCiantar">
+          <li className="hover:text-black">
             <SiGithub size="2rem" />
           </li>
         </a>
-        <a href="">
-          <li>
+        <a href="mailto:benjamin.ciantar@hyperisland.se">
+          <li className="hover:text-black">
             <BsEnvelope size="2.3rem" />
           </li>
         </a>
