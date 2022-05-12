5d31983c6e37dacf93f0729a240c93f4e6c1e189
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-05-12 12:13:33 +0200

    Added basic styling and layout to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Title goes here</h1>
+    <header className="w-screen h-20 bg-primary-purple text-white flex flex-row justify-between items-center px-7">
+      <h1 className="text-3xl">Hi, I'm Ben</h1>
+      <nav>NAV ICONS HERE</nav>
     </header>
   );
 };
