7efb8e102f14fadc69b30fe1b51f32fd77cb8014
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-05-17 22:21:36 +0200

    Updated about me text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
     <section className="w-full h-fit pt-10 text-black flex flex-col justify-around items-center">
       <h2 className="text-2xl pb-6">I'm a Frontend Developer</h2>
       <p className="text-lg w-3/4 text-justify">
-        Australian living in Stockholm. Endlessly curious about new
-        technologies. Enjoys building responsive single page apps in React more
+        Australian living in Stockholm. Endlessly tinkering with new
+        technologies. Enjoying building responsive single page apps in React more
         than a well adjusted human should. Planning to go full stack in the
         future. Let's chat?
       </p>
